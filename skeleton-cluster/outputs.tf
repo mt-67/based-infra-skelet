332e263bdae02348e1d7960b36ed408c75673cbf
Matvey <matvey@MacBook-Air-Matvey.local>
2025-05-26 21:34:31 +0200

    add new outputs

--- skeleton-cluster/outputs.tf
+++ skeleton-cluster/outputs.tf
@@ -5,3 +5,11 @@ output "vpc_id" {
 output "eks_cluster_name" {
   value = module.eks.cluster_name
 }
+
+output "private_subnets" {
+  value = module.vpc.private_subnets
+}
+
+output "public_subnets" {
+  value = module.vpc.public_subnets
+}
