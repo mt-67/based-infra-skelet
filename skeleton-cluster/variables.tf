4bef6e84da82169351363077762df4a9fafafd07
Matvey <matvey@MacBook-Air-Matvey.local>
2025-05-26 21:28:26 +0200

    add new variables.tf and update main.tf

--- skeleton-cluster/variables.tf
+++ skeleton-cluster/variables.tf
@@ -3,3 +3,39 @@ variable "aws_region" {
   type        = string
   default     = "eu-west-3"
 }
+
+variable "vpc_name" {
+  description = "VPC name"
+  type        = string
+  default     = "katana-foundation"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "azs" {
+  description = "list AZ"
+  type        = list(string)
+  default     = ["eu-west-3a", "eu-west-3b", "eu-west-3c"]
+}
+
+variable "public_subnets" {
+  description = "public subnet CIDR"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+}
+
+variable "private_subnets" {
+  description = "private subnet CIDR"
+  type        = list(string)
+  default     = ["10.0.10.0/24", "10.0.11.0/24", "10.0.12.0/24"]
+}
+
+variable "enable_nat" {
+  description = "enable NAT"
+  type        = bool
+  default     = false
+}
