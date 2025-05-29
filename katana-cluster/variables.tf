c0b852c4d2bb166685173a4dc16c63d3fe816a08
Matvey <matvejo63@gmail.com>
2025-05-29 17:55:41 +0200

    Update variables.tf

--- katana-cluster/variables.tf
+++ katana-cluster/variables.tf
@@ -3,39 +3,3 @@ variable "aws_region" {
   type        = string
   default     = "eu-west-3"
 }
-
-variable "vpc_name" {
-  description = "VPC name"
-  type        = string
-  default     = "Skeleton"
-}
-
-variable "vpc_cidr" {
-  description = "CIDR block for the VPC"
-  type        = string
-  default     = "10.0.0.0/16"
-}
-
-variable "public_subnets" {
-  description = "public subnet CIDR"
-  type        = list(string)
-  default     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-}
-
-variable "private_subnets" {
-  description = "private subnet CIDR"
-  type        = list(string)
-  default     = ["10.0.10.0/24", "10.0.11.0/24", "10.0.12.0/24"]
-}
-
-variable "azs" {
-  description = "list AZ"
-  type        = list(string)
-  default     = ["eu-west-3a", "eu-west-3b", "eu-west-3c"]
-}
-
-variable "enable_nat" {
-  description = "enable NAT"
-  type        = bool
-  default     = false
-}
