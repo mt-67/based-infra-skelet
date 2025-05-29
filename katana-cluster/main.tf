07f5c678c4ffd81f7787df16d3a40d1b64a505f8
Matvey <matvejo63@gmail.com>
2025-05-29 16:42:57 +0200

    Update main.tf

--- katana-cluster/main.tf
+++ katana-cluster/main.tf
@@ -37,3 +37,12 @@ module "eks" {
     Environment = "katana-env"
   }
 }
+
+resource "aws_cloudwatch_log_group" "vpc_logs" {
+  name              = "/vpc/flow-logs"
+  retention_in_days = 30
+
+  tags = {
+    Name = "VPC Flow Logs"
+  }
+}
