c36bcc6ad715ce90eac018e61aa3aea2b524d72f
Matvey <matvejo63@gmail.com>
2025-05-29 18:01:25 +0200

    Update main.tf

--- katana-cluster/main.tf
+++ katana-cluster/main.tf
@@ -1,30 +1,3 @@
-module "vpc" {
-  source  = "terraform-aws-modules/vpc/aws"
-  version = "~> 5.0"
-
-  name = var.vpc_name
-  cidr = var.vpc_cidr
-
-  azs             = var.azs
-  public_subnets  = var.public_subnets
-  private_subnets = var.private_subnets
-
-  enable_nat_gateway   = var.enable_nat
-  single_nat_gateway   = true
-  one_nat_gateway_per_az = false
-
-  tags = {
-    Project = "katana"
-  }
-}
-
-
-
-  tags = {
-    Environment = "katana-env"
-  }
-}
-
 resource "aws_cloudwatch_log_group" "vpc_logs" {
   name              = "/vpc/flow-logs"
   retention_in_days = 30
