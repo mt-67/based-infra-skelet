d2d70c01d2be953cfbe9157c2826bb9ea7224349
Matvey <matvejo63@gmail.com>
2025-05-29 16:47:24 +0200

    Update irsa.tf

--- katana-cluster/irsa.tf
+++ katana-cluster/irsa.tf
@@ -33,6 +33,41 @@ resource "aws_iam_policy" "logs_writer" {
   })
 }
 
+resource "aws_iam_role" "flow_logs_role" {
+  name = "vpc-flow-logs-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect = "Allow",
+      Principal = {
+        Service = "vpc-flow-logs.amazonaws.com"
+      },
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy" "flow_logs_policy" {
+  name = "vpc-flow-logs-policy"
+  role = aws_iam_role.flow_logs_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect = "Allow",
+      Action = [
+        "logs:CreateLogGroup",
+        "logs:CreateLogStream",
+        "logs:PutLogEvents",
+        "logs:DescribeLogGroups",
+        "logs:DescribeLogStreams"
+      ],
+      Resource = "*"
+    }]
+  })
+}
+
 resource "aws_iam_role_policy_attachment" "logs_writer_attach" {
   role       = aws_iam_role.irsa.name
   policy_arn = aws_iam_policy.logs_writer.arn
