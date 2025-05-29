ae4e091592095a3b5fa06fd3933958a43d000a57
Matvey <matvejo63@gmail.com>
2025-05-29 16:28:17 +0200

    Create irsa.tf

---
+++ katana-cluster/irsa.tf
@@ -0,0 +1,51 @@
+resource "aws_iam_role" "irsa" {
+  name = "irsa"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Federated = aws_iam_openid_connect_provider.eks.arn
+      }
+      Action = "sts:AssumeRoleWithWebIdentity"
+      Condition = {
+        StringEquals = {
+          "${replace(aws_iam_openid_connect_provider.eks.url, "https://", "")}:sub" = "system:serviceaccount:default:logs-writer"
+        }
+      }
+    }]
+  })
+}
+
+resource "aws_iam_policy" "logs_writer" {
+  name        = "logs-writer"
+  description = "Allow writing to CloudWatch Logs"
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect = "Allow"
+      Action = [
+        "logs:CreateLogStream",
+        "logs:PutLogEvents"
+      ]
+      Resource = "*"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "logs_writer_attach" {
+  role       = aws_iam_role.irsa.name
+  policy_arn = aws_iam_policy.logs_writer.arn
+}
+
+resource "kubernetes_service_account" "logs_writer" {
+  metadata {
+    name      = "logs-writer"
+    namespace = "default"
+    annotations = {
+      "eks.amazonaws.com/role-arn" = aws_iam_role.irsa.arn
+    }
+  }
+
+  # TODO: Attach this SA to a pod via spec.serviceAccountName: logs-writer
+}
