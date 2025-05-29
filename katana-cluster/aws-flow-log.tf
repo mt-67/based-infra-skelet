e7e25af83e32840f52328548d859d7b85d01d186
Matvey <matvejo63@gmail.com>
2025-05-29 16:50:11 +0200

    Update aws-flow-log.tf

--- katana-cluster/aws-flow-log.tf
+++ katana-cluster/aws-flow-log.tf
@@ -1,7 +1,1 @@
-resource "aws_flow_log" "vpc" {
-  log_destination      = aws_cloudwatch_log_group.vpc_logs.arn
-  iam_role_arn         = aws_iam_role.flow_logs_role.arn
-  traffic_type         = "ALL"
-  vpc_id               = module.vpc.vpc_id
-  log_format           = "${timestamp} ${srcaddr} ${dstaddr}"
-}
+
